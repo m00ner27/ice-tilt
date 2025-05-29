21586906550d7c8b6444e88a25447b7fca3355d5
“Kieran <kieran.r.moon@gmail.com>
2025-05-29 23:35:19 +0100

    fixed after royally fucking up

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiPlayerProfilesUrl: 'http://localhost:3000/api/player-profiles',
-  apiUrl: 'http://localhost:3001',
-  apiAudience: 'http://localhost:3000',
-  apiAllowedList: [
-    'http://localhost:3000/api/*'
-  ]
-};
